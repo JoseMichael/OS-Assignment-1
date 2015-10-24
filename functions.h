425582e8ca3cd1e1da6c2dd68aecdc008ac65b15
JoseMichael <vegroth333@gmail.com>
2015-10-24 04:01:56 -0400

    Updated header file with all function protos

--- functions.h
+++ functions.h
@@ -3,4 +3,12 @@
 
 int max(int size, int *intarray);  
 
+void multiply(int *a, int *b, int n, int m, int l, int *c);
+
+int wc(char str[]);
+
+void quicksort(int* array, int low, int high);
+
+int * sort(int size, int* array);
+
 #endif // FUNCTIONS_H_
