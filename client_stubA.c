30af2b29481a2b4838cb2e7d7dac0a028a6d262a
bhavinmodi <bhavin_modi@hotmail.com>
2015-10-28 01:52:18 -0400

    Client_stubA.c Updated
    Added Directory Service to client stub. Need to test to see if IP is
    correctly set and can connect to server.

--- client_stubA.c
+++ client_stubA.c
@@ -6,6 +6,17 @@
 //Globals
 int sock;
 
+int sendAck(int sock)
+{
+	int ackValue = 1, statusOfAck;
+	statusOfAck = send(sock, &ackValue, sizeof(int), 0);
+	if(statusOfAck > 0){
+		return 1;
+	}else{
+		return -1;
+	}
+}
+
 int sendToServer(int sock, int typeOfData, int *intArray, int integerToSend, char *stringToSend, int sizeOfData){
 	/*
 	basic type assumptions for this code
@@ -324,6 +335,7 @@ int * sort(int size, int* intarray){
 
 int main(){
 
+	int statusOfSend, statusOfRead;
     struct sockaddr_in server;
     
 	//Create socket
@@ -335,21 +347,105 @@ int main(){
     }
     puts("Socket created");
 
+	//Connect to Directory Register and get ip and port for service
 	server.sin_addr.s_addr = inet_addr("127.0.0.1");
     server.sin_family = AF_INET;
-    server.sin_port = htons( 8888 );
+    server.sin_port = htons( 8000 );
 	
-	//Connect to Server
-	puts("Trying to connect\n");
+	//Connect to Directory Service
+	puts("Trying to connect to Directory Service");
 	int connected = -1;
 	while(connected == -1){
-		//Connect to remote server
     	connected = connect(sock , (struct sockaddr *)&server , sizeof(server));
 	}
     
-	//Connected to server
-    puts("Connected\n");
+	//Connected to Directory Service
+    puts("Connected");
 	
+	//Let Directory know you are the client
+	statusOfSend = sendToServer(sock, 1, NULL, 0, NULL, 0);
+	if(statusOfSend < 0){
+		puts("Send programID failed");
+		close(sock);
+		return 0;
+	}
+	
+	//Send Lookup Request
+	int ip[4];
+	int port;
+	
+	//Send Program ID
+	statusOfSend = sendToServer(sock, 1, NULL, programID, NULL, 0);
+	if(statusOfSend < 0){
+		puts("Send programID failed");
+		close(sock);
+		return 0;
+	}
+	
+	//Send Version
+	statusOfSend = sendToServer(sock, 1, NULL, version, NULL, 0);
+	if(statusOfSend < 0){
+		puts("Send Version failed");
+		close(sock);
+		return 0;
+	}
+	
+	//Get IP
+	statusOfRead = recv(sock , &ip , sizeof(int)*4,0);
+	if(statusOfRead < 0){
+		puts("Receive IP failed");
+		close(sock);
+		return 0;
+	}
+	
+	//Send the ACK
+	if(sendAck(sock) < 0){
+		puts("Send ACK failed");
+		close(sock);
+		return 0;
+	}
+
+	//Get Port
+	statusOfRead = recv(sock , &port , sizeof(port),0);
+	if(statusOfRead < 0){
+		puts("Receive Port failed");
+		close(sock);
+		return 0;
+	}
+
+	//Convert ip to string
+	char address[50];
+	sprintf(address, "%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3]);
+
+	//Trim the char array
+	
+	//Close connection to Directory Service
+	close(sock);
+	
+	//Create socket
+    sock = socket(AF_INET , SOCK_STREAM , 0);
+    if (sock == -1)
+    {
+        puts("Could not create socket");
+		return 1;
+    }
+    puts("Socket created");
+	
+	//Now setup Server connection
+	server.sin_addr.s_addr = inet_addr(address);
+    server.sin_family = AF_INET;
+    server.sin_port = htons( port );
+	
+	//Connect to Remote Server
+	puts("Trying to connect to Remote Server");
+	connected = -1;
+	while(connected == -1){
+    	connected = connect(sock , (struct sockaddr *)&server , sizeof(server));
+	}
+    
+	//Connected to Remote Server
+    puts("Connected");
+
 	while(1){
 		//Call input in client file
 		if(input() == -1){
