b48094047506e829d686f631750d3a833b49cdd4
bhavinmodi <bhavin_modi@hotmail.com>
2015-10-29 19:59:42 -0400

    File needed for server stub generation.

---
+++ includeserverfiles.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<pthread.h>
+#include "rpcheader.h"
+
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <net/if.h>
